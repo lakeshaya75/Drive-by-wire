96350d0fe24e4b7373da477aad9710ae7f06b87e
lakeshaya75 <135804843+lakeshaya75@users.noreply.github.com>
2024-07-23 17:24:19 -0700

    rtc

--- Drive_By_Wire/Vehicle.cpp
+++ Drive_By_Wire/Vehicle.cpp
@@ -57,20 +57,6 @@ Vehicle::Vehicle() {
   desired_brake = 0;
   desired_angle = 0;
 
-   
-  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
-  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
-  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
-  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
-  // float drift_unit = 4.069; //For corrections every min the drift_unit is 4.069 ppm (use with offset mode PCF8523_OneMinute)
-  int offset = round(deviation_ppm / drift_unit);
-  // rtc.calibrate(PCF8523_TwoHours, offset); // Un-comment to perform calibration once drift (seconds) and observation period (seconds) are correct
-  // rtc.calibrate(PCF8523_TwoHours, 0); // Un-comment to cancel previous calibration
-
-  Serial.print("Offset is "); 
-  Serial.println(offset); // Print to control offset
-  Serial.flush();
-
 #if DBWversion < 4
   // Keep trying to initialize CAN
   while (!CAN.begin(CAN_500KBPS)) { // changed to false For testing purposes CAN.begin(CAN_500KBPS) ReEnable for DBWV4
@@ -118,6 +104,19 @@ void Vehicle::initalize(){
   Serial.println("RTC time set to compile time.");
 
   rtc.start();
+
+  float drift = 43; // seconds plus or minus over oservation period - set to 0 to cancel previous calibration.
+  float period_sec = (7 * 86400);  // total obsevation period in seconds (86400 = seconds in 1 day:  7 days = (7 * 86400) seconds )
+  float deviation_ppm = (drift / period_sec * 1000000); //  deviation in parts per million (μs)
+  float drift_unit = 4.34; // use with offset mode PCF8523_TwoHours
+  // float drift_unit = 4.069; //For corrections every min the drift_unit is 4.069 ppm (use with offset mode PCF8523_OneMinute)
+  int offset = round(deviation_ppm / drift_unit);
+  // rtc.calibrate(PCF8523_TwoHours, offset); // Un-comment to perform calibration once drift (seconds) and observation period (seconds) are correct
+  // rtc.calibrate(PCF8523_TwoHours, 0); // Un-comment to cancel previous calibration
+
+  Serial.print("Offset is "); 
+  Serial.println(offset); // Print to control offset
+  Serial.flush();
 
 #if DBWversion < 4
   bool parse=false;
